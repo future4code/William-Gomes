a2e7af119c0a27d90f97ff49087806fa6046eae9
William Gomes <76756233+WilliamGofe@users.noreply.github.com>
2021-05-18 15:25:18 -0300

    introdução ao typescript

---
+++ semana12/introducao Typescript/src/exercicio-4.ts
@@ -0,0 +1,26 @@
+
+/*const pokemons = {
+     pokemon1 = {
+        name: "Charmander",
+        types: "Fire",
+        healthPoints: 28
+      },
+      
+       pokemon2 = {
+        name: "Bulbasaur",
+        types: "Grass/Poison",
+        healthPoints: 31
+      },
+      
+      pokemon3 = {
+        name: "Squirtle",
+        types: "Water",
+        healthPoints: 35
+      }
+}*/
+
+//Sim, estariamos compilando o node do arquivo descrevendo a rota para chegar nele
+
+//dando o comando tsc dentro do aquivo src 
+
+// A versão do EcmaScript
